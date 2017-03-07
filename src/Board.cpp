1d132a10583fe06bf79ddb6f102d2a530cc5b218
Mahmoud Shokry <mshokry11@gmail.com>
2017-03-07 17:38:42 +0200

    added constructors to class Piece

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,15 +4,16 @@
  *  Created on: Mar 7, 2017
  *      Author: reem
  */
+
 #include "Board.h"
 #include "Cell.h"
-#include "Piece.h"
 #include "Pawn.h"
 #include "Rook.h"
 #include "Knight.h"
 #include "Bishop.h"
 #include "King.h"
 #include "Queen.h"
+#include "Color.h"
 
 enum {
 	BLACK_PAWN1,
@@ -61,39 +62,39 @@ Board::Board() {
 	// pawns
 	for (int i = WHITE_PAWN1; i <= WHITE_PAWN8; ++i) {
 		int idx = i - WHITE_PAWN1;
-		pieces[i] = new Pawn(cells[6][idx]);
+		pieces[i] = new Pawn(cells[6][idx], WHITE);
 	}
 	for (int i = BLACK_PAWN1; i <= BLACK_PAWN8; ++i) {
 		int idx = i - BLACK_PAWN1;
-		pieces[i] = new Pawn(cells[1][idx]);
+		pieces[i] = new Pawn(cells[1][idx], BLACK);
 	}
 
 	// rooks
-	pieces[WHITE_ROOK1] = new Rook(cells[7][0]);
-	pieces[WHITE_ROOK2] = new Rook(cells[7][7]);
-	pieces[BLACK_ROOK1] = new Rook(cells[0][0]);
-	pieces[BLACK_ROOK2] = new Rook(cells[0][7]);
+	pieces[WHITE_ROOK1] = new Rook(cells[7][0], WHITE);
+	pieces[WHITE_ROOK2] = new Rook(cells[7][7], WHITE);
+	pieces[BLACK_ROOK1] = new Rook(cells[0][0], BLACK);
+	pieces[BLACK_ROOK2] = new Rook(cells[0][7], BLACK);
 
 
 	// knights
-	pieces[WHITE_KNIGHT1] = new Knight(cells[7][1]);
-	pieces[WHITE_KNIGHT2] = new Knight(cells[7][6]);
-	pieces[BLACK_KNIGHT1] = new Knight(cells[0][1]);
-	pieces[BLACK_KNIGHT2] = new Knight(cells[0][6]);
+	pieces[WHITE_KNIGHT1] = new Knight(cells[7][1], WHITE);
+	pieces[WHITE_KNIGHT2] = new Knight(cells[7][6], WHITE);
+	pieces[BLACK_KNIGHT1] = new Knight(cells[0][1], BLACK);
+	pieces[BLACK_KNIGHT2] = new Knight(cells[0][6], BLACK);
 
 	// bishops
-	pieces[WHITE_BISHOP1] = new Bishop(cells[7][2]);
-	pieces[WHITE_BISHOP2] = new Bishop(cells[7][5]);
-	pieces[BLACK_BISHOP1] = new Bishop(cells[0][2]);
-	pieces[BLACK_BISHOP2] = new Bishop(cells[0][5]);
+	pieces[WHITE_BISHOP1] = new Bishop(cells[7][2], WHITE);
+	pieces[WHITE_BISHOP2] = new Bishop(cells[7][5], WHITE);
+	pieces[BLACK_BISHOP1] = new Bishop(cells[0][2], BLACK);
+	pieces[BLACK_BISHOP2] = new Bishop(cells[0][5], BLACK);
 
 	// kings
-	pieces[WHITE_KING] = new King(cells[7][4]);
-	pieces[BLACK_KING] = new King(cells[0][3]);
+	pieces[WHITE_KING] = new King(cells[7][4], WHITE);
+	pieces[BLACK_KING] = new King(cells[0][3], BLACK);
 
 	// queens
-	pieces[WHITE_QUEEN] = new Queen(cells[7][3]);
-	pieces[BLACK_QUEEN] = new Queen(cells[0][4]);
+	pieces[WHITE_QUEEN] = new Queen(cells[7][3], WHITE);
+	pieces[BLACK_QUEEN] = new Queen(cells[0][4], BLACK);
 
 }
 
--- src/Piece.h
+++ src/Piece.h
@@ -9,11 +9,15 @@
 #define PIECES_H_
 
 #include "Cell.h"
+#include "Color.h"
 
 class Piece {
 private:
 	Cell *current_cell;
+	Color color;
 public:
+	Piece();
+	Piece(Cell *cell, Color color);
 	bool isValid(Cell to);
 	void setCurrentCell(Cell cell);
 	Cell* getCurrentCell();
